632183fe56b58429387eea799b7085f97f0465de
pratibhanikamkatkar <pratibhanikam98@yahoo.com>
2025-06-30 17:23:47 +0530

    first commit

---
+++ exceptionHandling2/src/MyCalcApp.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class MyArithmeticException extends Exception{
+    public MyArithmeticException(String message){
+        super(message);
+    }
+}
+
+class Calculator{
+    public double caldouble(int num) throws MyArithmeticException {
+        if(num==0){
+            throw new MyArithmeticException("Zero not allowed");
+        } else if (num<0) {
+            throw new MyArithmeticException("Negative not allowed");
+        }
+        else {
+            return num * 2.0;
+        }
+    }
+}
+
+public class MyCalcApp{
+    public static void main(String[] args){
+        Calculator cal=new Calculator();
+        try {
+            double result =cal.caldouble(1);
+            System.out.println("Double of number is "+ result);
+        } catch (MyArithmeticException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+}
